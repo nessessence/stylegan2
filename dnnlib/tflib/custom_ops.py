780d8d8006d5ff901b03128e2ca95bad9d28d6b5
nessessence <nessessence@email.com>
2021-10-07 20:55:26 +0700

    gpu compat

--- dnnlib/tflib/custom_ops.py
+++ dnnlib/tflib/custom_ops.py
@@ -47,9 +47,7 @@ def _get_compute_cap(device):
     return (major, minor)
 
 def _get_cuda_gpu_arch_string():
-    import os
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0' 
-    gpus = [x for x in device_lib.list_local_devices() if  (x.device_type == 'GPU' or x.device_type == 'XLA_GPU') ]
+    gpus = [x for x in device_lib.list_local_devices() if  x.device_type == 'GPU' ]
     if len(gpus) == 0:
         raise RuntimeError('No GPU devices found')
     (major, minor) = _get_compute_cap(gpus[0])
